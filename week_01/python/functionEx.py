66603bf7d8cd3d2c8f32a31378f4eafc88c5c7dc
lee <hansollee333@gmail.com>
2025-03-15 17:22:05 +0900

    　

--- week_01/python/functionEx.py
+++ week_01/python/functionEx.py
@@ -1,7 +1,7 @@
 # 'print_3_time' 함수 정의: "안녕하세요"를 3번 출력하고 여러 값을 반환합니다.
 def print_3_time():
     for i in range(3):  # range(3)은 0, 1, 2 3번 반복합니다.
-        print("안녕하세요")  # "안녕하세요"를 출력합니다.
+        print("(3 time 함수 실행)안녕하세요")  # "안녕하세요"를 출력합니다.
     
     a = 100  # 변수 'a'에 100을 저장합니다.
     return "ok", 1, "complete", a  # 여러 개의 값을 반환합니다. 반환값은 튜플로 묶입니다.
@@ -9,7 +9,7 @@ def print_3_time():
 # 'print_n_time' 함수 정의: 'n'번 만큼 "안녕하세요"를 출력합니다.
 def print_n_time(n):
     for i in range(n):  # n번 반복합니다. n은 매개변수로 전달됩니다.
-        print("안녕하세요")  # "안녕하세요"를 출력합니다.
+        print("(n time 함수 실행)안녕하세요")  # "안녕하세요"를 출력합니다.
 
 # main 함수 정의: 전체 프로그램의 흐름을 제어합니다.
 def main():
