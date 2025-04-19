48a7393a7cc04fb3af05232aff3fc1660f55c430
Lee <hansollee333@gmail.com>
2025-04-19 08:29:05 +0000

    　

--- catkin_ws/src/hello_ros/scripts/hello.py
+++ catkin_ws/src/hello_ros/scripts/hello.py
@@ -4,18 +4,26 @@ import rospy
 from std_msgs.msg import String
 
 
+class Hello:
+    def __init__(self):
+        self.pub = rospy.Publisher('message', String, queue_size=10)
+        self.data = String()
+        self.i=0
+        rospy.Timer(rospy.Duration(0.33), self.print_hello)
+        self.rate = rospy.Rate(3)
+
+    def print_hello(self, _event):
+        self.data.data = f"hello, ROS! noetic {self.i}"
+        rospy.loginfo(self.data.data)
+        self.pub.publish(self.data)
+        self.i += 1
+
+
 def main():
     rospy.init_node('hello', anonymous=True)
-    pub = rospy.Publisher('message', String, queue_size=10)
-    data = String()
-    i=0
-    rate = rospy.Rate(3)
+    node = Hello()
     while not rospy.is_shutdown():
-        data.data = f"hello, ROS! noetic {i}"
-        pub.publish(data)
-        print("hello, ROS1 noetic!!")
-        rate.sleep()
-        i += 1
+        rospy.spin()
 
 if __name__ == "__main__":
     try:
