6c00d03c2c8aab802cbf2918498f2a3d373eb320
Lee <hansollee333@gmail.com>
2025-04-19 11:10:24 +0900

    　

---
+++ colcon_ws/src/install/hello_ros2/lib/python3.10/site-packages/hello_ros2/hello_ros.py
@@ -0,0 +1,34 @@
+# ros2 pkg create --build-type ament_python hello_ros2
+# code ~/.bashrc
+# cd colcon_ws/
+# colcon build
+# source install/local_setup.bash 
+# ros2 run hello_ros2 hello_ros 
+# ros2 pkg list | grep hello
+
+
+import rclpy
+from rclpy.node import Node
+
+
+class Hello(Node):
+    def __init__(self):
+        super().__init__('hello')
+        self.create_timer(1, self.print_hello)
+        self.count = 0
+
+    def print_hello(self):
+        print(f"hello, ROS2 humble!! {self.count}")
+        self.count += 1
+
+
+def main():
+    rclpy.init()
+    node = Hello()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.destroy_node()
+
+if __name__== '__main__':
+    main()
