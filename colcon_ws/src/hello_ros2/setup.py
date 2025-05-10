68a63fa1327c054af18cb33c81aa787ecc2ae716
Lee <hansollee333@gmail.com>
2025-05-10 10:29:03 +0900

    새 파일:       colcon_ws/src/hello_ros2/hello_ros2/simple_service_server2.py 	수정함:        colcon_ws/src/hello_ros2/setup.py

--- colcon_ws/src/hello_ros2/setup.py
+++ colcon_ws/src/hello_ros2/setup.py
@@ -27,7 +27,8 @@ setup(
             'move_turtle = hello_ros2.move_turtle:main',
             'simple_sub = hello_ros2.simple_sub:main',
             'simple_pub = hello_ros2.simple_pub:main',
-            'simple_service_server = hello_ros2.simple_service_server:main'
+            'simple_service_server = hello_ros2.simple_service_server:main',
+            'simple_service_server2 = hello_ros2.simple_service_server2:main',
         ],
     },
 )
