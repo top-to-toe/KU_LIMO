2d8bd12eb75e8532c262c6cfaccd7425f197c43e
Lee <hansollee333@gmail.com>
2025-05-10 14:28:48 +0900

    turtlesim.yaml 및 moveTurtle.launch.py 파일 추가

---
+++ colcon_ws/src/hello_ros2/launch/moveTurtle.launch.py
@@ -0,0 +1,29 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    param_dir = LaunchConfiguration(
+        "param_dir",
+        default=os.path.join(
+            get_package_share_directory("hello_ros2"), "param", "turtlesim.yaml"
+        ),
+    )
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "param_dir",
+                default_value=param_dir,
+                description="turtlesim paramdter dump file",
+            ),
+            Node(
+                package="turtlesim", executable="turtlesim_node", parameters=[param_dir]
+            ),
+            Node(package="hello_ros2", executable="move_turtle"),
+        ]
+    )
