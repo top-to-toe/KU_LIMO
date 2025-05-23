d05f7dc0dbe70deda4a4ea29d90b76523e68d304
Lee <hansollee333@gmail.com>
2025-05-23 22:04:22 +0900

    　

--- colcon_ws/src/hello_ros2/setup.py
+++ colcon_ws/src/hello_ros2/setup.py
@@ -24,7 +24,7 @@ setup(
     install_requires=["setuptools"],
     zip_safe=True,
     maintainer="lee",
-    maintainer_email="hansollee333@gmail.com",
+    maintainer_email="lee@todo.todo",
     description="TODO: Package description",
     license="TODO: License declaration",
     tests_require=["pytest"],
