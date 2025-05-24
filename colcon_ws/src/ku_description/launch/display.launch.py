bf220b1e34a1b88b5da62fa246a8df6e74302813
Lee <hansollee333@gmail.com>
2025-05-24 10:20:36 +0900

    display.launch.py 파일 수정: 불필요한 노드 제거 및 urdf_package 오타 수정

--- colcon_ws/src/ku_description/launch/display.launch.py
+++ colcon_ws/src/ku_description/launch/display.launch.py
@@ -14,14 +14,12 @@ def generate_launch_description():
     return LaunchDescription(
         [
             model,
-            Node(package="hello_ros2", executable="simple_pub"),
-            Node(package="hello_ros2", executable="simple_sub"),
             IncludeLaunchDescription(
                 PathJoinSubstitution(
                     [FindPackageShare("urdf_launch"), "launch", "display.launch.py"]
                 ),
                 launch_arguments={
-                    "urdf_packge": "ku_description",
+                    "urdf_package": "ku_description",
                     "urdf_package_path": LaunchConfiguration("model"),
                 }.items(),
             ),
